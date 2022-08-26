3aee0a95c7a9710f49dd6e35f58f661bef1a5cee
Delta456 <swstkbaranwal@gmail.com>
2022-08-26 22:04:01 +0530

    add tests

---
+++ vlib/v/tests/reference_operator_overloading_test.v
@@ -0,0 +1,39 @@
+struct Generic<T> {
+	f T
+}
+
+struct NonGeneric {
+	f int
+}
+
+fn (f &Generic<T>) + (f1 &Generic<T>) &Generic<T> {
+	return &Generic<T>{
+		f: f.f + f1.f
+	}
+}
+
+fn (f &NonGeneric) + (f1 &NonGeneric) &NonGeneric {
+	return &NonGeneric{
+		f: f.f + f1.f
+	}
+}
+
+fn test_referenced_generic_operator_overloading() {
+	mut generic := &Generic<int>{2}
+	generic += &Generic<int>{6}
+
+	assert generic.f == 8
+	assert (generic + &Generic<int>{2}).f == 10
+}
+
+fn test_referenced_operator_overloading() {
+	mut non_generic := &NonGeneric{2}
+
+	// TODO: make the below code to compile
+	// non_generic += &NonGeneric{6}
+	// assert non_generic.f == 8
+
+	assert (non_generic + &NonGeneric{2}).f == 4
+
+}
+
